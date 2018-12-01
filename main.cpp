7cdf56e7646b2385b65f97a48fef69e02ce4a80d
quantrancse <trungquan412@gmail.com>
2018-12-01 19:10:05 +0700

    Update

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include "Calculate.h"
 #include "Calculate.cpp"
+#include "Calculate.h"
 
 using namespace std;
 
 int main() {
     string str;
+    Calculate s;
+    
     cout << "Enter equation: ";
     getline(cin, str);
-
-    Calculate s;
     s.eval(str);
 
+    // system("pause");
     return 0;
 }
